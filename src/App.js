0c1b6c0c46af46e8de59971411e03f231aac7200
Troy Kuhn <troykuhn22@gmail.com>
2020-08-08 02:22:03 -0400

    recipe search

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 import Auth from './components/Auth/Auth';
-import Items from './components/Items/Items';
+import Recipe from './components/Recipe/Recipe';
+import Items from './components/Items/items';
 import OurNav from './components/Navbar/Navbar';
+// import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
 
+  //Recipe Variables
+  const appId = "b039b354";
+  const appKey = "b5870411f65402898f8b6516bbf042eb";
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState('chicken');
+
+  const exampleRequest = `https://api.edamam.com/search?q=${query}&app_id=${appId}&app_key=${appKey}`;
+
+  useEffect ( () => {
+    getRecipes();
+  }, [query]);
+
+  const getRecipes = async () => {
+    const response = await fetch(exampleRequest);
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+  }
+
+  const updateSearch = (event) => {
+    setSearch(event.target.value);
+  }
+
+  const getSearch = event => {
+    event.preventDefault();
+    setQuery(search);
+    setSearch('');
+  }
+
   const [sessionToken, setSessionToken] = useState(undefined);
-  console.log(sessionToken);
+  sessionToken === undefined ? console.log('No user signed in.') : console.log(`Your session token is: ${sessionToken}`) ;
 
   const viewConductor = () => {
     return sessionToken !== undefined ?
@@ -26,10 +58,30 @@ function App() {
     setSessionToken(undefined);
   }
 
+  
+
   return (
     <div className="App">
       <OurNav clearToken={clearToken}/>
       {viewConductor()}
+
+      { /*Recipe*/ }
+      <div id="recipes">
+        <h3>Search Recipes</h3>
+        <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
+        <button className="search-button" type="submit">Search</button>
+      </form>
+      {recipes.map(recipe => (
+        <Recipe 
+        key={recipe.recipe.label} 
+        title={recipe.recipe.label} 
+        calories={recipe.recipe.calories} 
+        image={recipe.recipe.image}
+        ingredients={recipe.recipe.ingredients} />
+      ))}
+      </div>
+
     </div>
   );
 }
