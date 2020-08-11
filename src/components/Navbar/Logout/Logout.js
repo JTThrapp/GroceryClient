8e5aa1f0ce5d1698a442d6d6c593112bb4939698
Carissa <carissapierce01@Yahoo.com>
2020-08-10 21:15:54 -0400

    nothing

--- src/components/Navbar/Logout/Logout.js
+++ src/components/Navbar/Logout/Logout.js
@@ -1,6 +1,4 @@
 <nav class="logout">
-        <ul>
           <li id="logoutt"> <a href="#logout" class="nav-linkk"> Grocery App </a> </li>
-          <li> <a href="#contact" class="nav-link"> Contact 
-        </ul>
-      </nav>
+          <li> <a href="#contact" class="nav-link"> Contact </a> </li>
+</nav>
